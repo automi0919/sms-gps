ecc3ccbba34e40d26e22afdbb81fecf0f8864448
YC Web Dev 2 <yc-webdev2@yinzcam.com>
2021-08-03 18:02:35 +0300

    change map stype

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -8,18 +8,64 @@ const Wrapper = styled.main`
   width: 100%;
   height: 100%;
 `
-
-const Map = ({ location, zoomLevel, children}) => (
+const createMapOptions = () => {
+  return {
+    styles: [
+      {
+        featureType: 'administrative',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#444444' }],
+      },
+      {
+        featureType: 'landscape',
+        elementType: 'all',
+        stylers: [{ color: '#f2f2f2' }],
+      },
+      {
+        featureType: 'poi',
+        elementType: 'all',
+        stylers: [{ visibility: '#off' }],
+      },
+      {
+        featureType: 'road',
+        elementType: 'all',
+        stylers: [{ saturation: '-100' }, { lightness: 45 }],
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'all',
+        stylers: [{ visibility: 'simplified' }],
+      },
+      {
+        featureType: 'road.arterial',
+        elementType: 'labels.icon',
+        stylers: [{ visibility: 'off' }],
+      },
+      {
+        featureType: 'transit',
+        elementType: 'all',
+        stylers: [{ visibility: 'off' }],
+      },
+      {
+        featureType: 'water',
+        elementType: 'all',
+        stylers: [{ color: '#46bcec' }, { visibility: 'on' }],
+      },
+    ],
+  }
+}
+const Map = ({ location, zoomLevel, children }) => (
   <Wrapper>
-    <div className="google-map">
+    <div className='google-map'>
       <GoogleMapReact
         bootstrapURLKeys={{ key: 'AIzaSyCO34vxnjcT_NlL8oP6BtF-A2E9AqN2u-k' }}
         center={location}
         zoom={zoomLevel}
+        options={createMapOptions}
       >
         {children}
       </GoogleMapReact>
     </div>
   </Wrapper>
 )
-export default Map;
+export default Map
