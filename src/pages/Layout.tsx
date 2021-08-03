5a72966052d82bf43f5a940a2ff3d368c9a86bb3
YC Web Dev 2 <yc-webdev2@yinzcam.com>
2021-08-03 21:31:07 +0300

    fix mark issue

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,13 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Map from '../components/Map/Map';
-import { currentGeoLocation } from '../store/sms/reducer';
+import { getCurrentGeoLocation } from '../store/sms/actions';
+import { approvedData, currentGeoLocation } from '../store/sms/reducer';
 import './style.css';
 
 const Layout  = (props:PropsType) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCurrentGeoLocation());
+  }, [])
   const markPos = useSelector(state => currentGeoLocation(state));
-  const markPosArray = [markPos];
+  const approvedPos = useSelector(state => approvedData(state));
+  const markPosArray = [markPos, approvedPos];
   return (
-    <div className="layout-container">
+    <div className="layout-container">yar
       <div className="map-wrapper">
         <Map location={markPos} zoomLevel={14} points={markPosArray}/>
       </div>
