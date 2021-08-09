fc98256cbead2520c32acf9987704fef744a25cb
WebStarDev <shopstar0907@gmail.com>
2021-08-09 09:49:38 -0400

    changed style

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,34 +7,39 @@ function About(props) {
   return (
     <Panel>
       <div className='privacy-container'>
-        <Scrollbars style={{ height: 400 }}>
-          <h3 className='title'>About</h3>
-          <p>
-            <strong>SAFE LOCATE</strong> is a simple NO APP & 2 TAP way of
-            requesting or sharing a location via SMS. A request link is sent to
-            a user's phone via SMS, the receiver simply taps the link and taps
-            ALLOW GPS on their phone and their location is sent back to the
-            requesting person or party.
-          </p>
-          <p>
-            <strong>SAFE LOCATE</strong> was a result of being first on the
-            scene at an accident 2 weeks ago and the man died in my arms. One of
-            the biggest issues was that the emergency services arrived 5-10
-            minutes late after driving past the location TWICE!
-          </p>
-          <p>
-            Initially, SL has been launched for FREE private use when trying to
-            locate a loved on when they have been involved in an incident, when
-            lost or to provide peace of mind. We will then be launching our
-            commercial offering which open up live location requests for
-            emergency services, client and employee tracking for businesses such
-            as insurance, roadside assist, delivery services etc.
-          </p>
-          <p>
-            <strong>HUGH CAMPBELL</strong>
-            <br /> FOUNDER
-          </p>
-        </Scrollbars>
+          <h2 className='title'>About</h2>
+          <article>
+            <p>
+              <strong>SAFE LOCATE</strong> is a simple <strong>NO APP & 2 TAP</strong> way of
+              requesting or sharing a location via SMS. A request link is sent to
+              a user's phone via SMS, the receiver simply taps the link and taps <strong>ALLOW GPS</strong> on their phone and their location is sent back to the
+              requesting person or party.
+            </p>
+          </article>
+          <article>
+            <p>
+              <strong>SAFE LOCATE</strong> was a result of being first on the
+              scene at an accident 2 weeks ago and the man died in my arms. <strong>One of
+              the biggest</strong> issues was that the emergency services arrived 5-10
+              minutes late after driving past the location <strong>TWICE</strong>!
+            </p>
+          </article>
+          <article>
+            <p>
+              Initially, SL has been launched for <strong>FREE</strong> private use when trying to
+              locate a loved on when they have been involved in an incident, when
+              lost or to provide peace of mind. We will then be launching our
+              commercial offering which open up live location requests for
+              emergency services, client and employee tracking for businesses such
+              as insurance, roadside assist, delivery services etc.
+            </p>
+          </article>
+          <article>
+            <p>
+              <strong>HUGH CAMPBELL</strong>
+              <br /> FOUNDER
+            </p>
+          </article>
       </div>
     </Panel>
   )
